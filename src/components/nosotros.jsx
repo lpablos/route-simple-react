ab80ef75712993642efd89cf71e0c9875acbdb8b
Luis Jorge P.S <lpablo@toshiba.com>
2021-04-27 23:42:07 -0500

    render of list with fecht petition

--- src/components/nosotros.jsx
+++ src/components/nosotros.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
 
 const nosotros = () => {
+  
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [equipo, setEquipo] = useState([]);
+    
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useEffect(()=>{
+        obtenerDatos()
+    }, [])
+
+    
+    const obtenerDatos = async () =>{
+        let data = await fetch('https://jsonplaceholder.typicode.com/users')
+        let usuarios = await data.json()
+        setEquipo(usuarios)
+    }
+
     return (
         <div>
-            <h3>Hola desde nosotros</h3>
+            <ul>
+                {
+                    equipo.map(team =>(
+                        <li key={team.id}>{team.name} - {team.username}</li>
+                    ))
+                }
+            </ul>
         </div>
     )
 }
 
 export default nosotros
+
